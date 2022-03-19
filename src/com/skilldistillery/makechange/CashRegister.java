22b31aa129371d9b99bb4dc2ea6ee7074ff88694
paul-chaffin <pechaffin@pm.me>
2022-03-18 21:14:47 -0400

    Fri EOD commit

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -4,40 +4,46 @@ import java.util.Scanner;
 
 public class CashRegister {
 
-	public static double amount = 0, tendered = 0, bulkChange = 0;
+	public static double amountSale = 0, amountTendered = 0, bulkChange = 0;
+	public static double prettyCoins, prettyBills;
 	public static int billsTen = 0, billsFive = 0, billsOne = 0;
 	public static int coinsQ = 0, coinsD = 0, coinsN = 0, coinsP = 0;
 
 	public static void main(String[] args) {
 
-//		getAmount();
-//		System.out.println(amount);
+		getAmount();
 
+		printFinalBills();
+		printFinalCoins();
 	}
 
 	public static void getAmount() {
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Enter the cost: > ");
-		amount = kb.nextDouble();
+		System.out.println("Enter the purchase cost: > ");
+		amountSale = kb.nextDouble();
+		kb.nextLine();
+		System.out.println("Enter your payment amount: > ");
+		amountTendered = kb.nextDouble();
 		kb.close();
-		getBills(bulkChange);
-		printFinalBills();
-		printFinalCoins();
+		getPayment();
 
 	}
 
 	public static void getPayment() {
-		if (tendered < amount) {
+		if (amountTendered < amountSale) {
 			System.out.println("Insufficient funds.");
-			break;
+		} else if (amountTendered == amountSale) {
+			System.out.println("No change due.");
 		} else {
-			bulkChange = amount - tendered;
+			bulkChange = amountTendered - amountSale;
+			prettyBills = (int) bulkChange;
+//			System.out.printf("Change due: $%s.%s", prettyBills, prettyCoins);
+//			TODO: FIX PRETTY PRINTING
 			getBills(bulkChange);
 		}
 	}
 
 	public static void getBills(double changeBills) {
-		changeBills = (int) (changeBills);
 		while (changeBills >= 10.0) {
 			billsTen++;
 			changeBills -= 10.0;
@@ -54,34 +60,39 @@ public class CashRegister {
 	}
 
 	public static void getCoins(double changeCoins) {
-		changeCoins = tendered % 10;
-		while (changeCoins > .25) {
+		changeCoins = (changeCoins % 10);
+		formatCoins(changeCoins);
+		while (changeCoins >= .25) {
 			coinsQ++;
 			changeCoins -= .25;
 		}
-		while (changeCoins > .10) {
+		while (changeCoins >= .10) {
 			coinsD++;
 			changeCoins -= .10;
 		}
-		while (changeCoins > .05) {
+		while (changeCoins >= .05) {
 			coinsN++;
 			changeCoins -= .05;
 		}
-		while (changeCoins > .01) {
+		while (changeCoins >= .01) {
 			coinsP++;
 			changeCoins -= .01;
 		}
 	}
 
+	public static void formatCoins(double uglyCoins) {
+		prettyCoins = (int) (uglyCoins * 100);
+	}
+
 	public static void printFinalBills() {
 		if (billsTen > 0) {
-			System.out.println("Tens: " + billsTen);
+			System.out.println("$10: " + billsTen);
 		}
 		if (billsFive > 0) {
-			System.out.println("Fives: " + billsFive);
+			System.out.println("$5: " + billsFive);
 		}
 		if (billsOne > 0) {
-			System.out.println("Ones: " + billsOne);
+			System.out.println("$1: " + billsOne);
 		}
 	}
 
@@ -89,17 +100,15 @@ public class CashRegister {
 		if (coinsQ > 0) {
 			System.out.println("25c: " + coinsQ);
 		}
-		if (billsOne > 0) {
+		if (coinsD > 0) {
 			System.out.println("10c: " + coinsD);
 		}
-		if (billsOne > 0) {
+		if (coinsN > 0) {
 			System.out.println("5c: " + coinsN);
 		}
-		if (billsOne > 0) {
+		if (coinsP > 0) {
 			System.out.println("1c: " + coinsP);
 		}
 	}
-
-	public static v
 
 }
---
+++ src/com/skilldistillery/makechange/Scratch.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.makechange;
+
+public class Scratch {
+	
+	public static void main(String[] args) {
+		
+//		double change = .33099999999;
+//		System.out.println(change);
+//		change = (int)(change * 100);
+//		System.out.println(change);
+		double bills = 20.13;
+		System.out.println(bills);
+		System.out.println(bills);
+		bills = bills % 1;
+		
+		
+	}
+	
+}
